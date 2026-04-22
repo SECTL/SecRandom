73f4e2c43ba752503f9de2ccb5f10a574a2aa439
lrs2187 <liurongshuo2022@outlook.com>
2026-04-22 13:42:38 +0800

    feat: 关于界面基本完工

--- SecRandom/Views/SettingsPages/AboutSettingsPage.axaml.cs
+++ SecRandom/Views/SettingsPages/AboutSettingsPage.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using FluentAvalonia.UI.Controls;
 using SecRandom.Core.Attributes;
 using SecRandom.Core.Enums;
 
@@ -10,5 +14,45 @@ public partial class AboutSettingsPage : UserControl
     public AboutSettingsPage()
     {
         InitializeComponent();
+    }
+
+    private void OpenLink(string url)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true
+            });
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            Process.Start(new ProcessStartInfo("xdg-open", url)
+            {
+                UseShellExecute = false
+            });
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            Process.Start(new ProcessStartInfo("open", url)
+            {
+                UseShellExecute = false
+            });
+        }
+    }
+    
+    private void UriNavigationCommands_OnClick(object sender, RoutedEventArgs e)
+    {
+        var url = e.Source switch
+        {
+            SettingsExpanderItem s => s.CommandParameter?.ToString(),
+            Button s => s.CommandParameter?.ToString(),
+            _ => null
+        };
+
+        if (url != null)
+        {
+            OpenLink(url);
+        }
     }
 }
