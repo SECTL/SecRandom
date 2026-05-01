8bd4e456a74a52bde2c73a881ede1396a006af5b
lrs2187 <liurongshuo2022@outlook.com>
2026-05-01 19:57:57 +0800

    feat: 恢复lottery的半重复

--- SecRandom.Core/Models/SubConfigs/LotterySettingsConfig.cs
+++ SecRandom.Core/Models/SubConfigs/LotterySettingsConfig.cs
@@ -5,6 +5,10 @@ namespace SecRandom.Core.Models.SubConfigs;
 
 public partial class LotterySettingsConfig : OverridableDrawSettings
 {
+    [ObservableProperty] private DrawMode _drawMode = DrawMode.NoRepeat;
+    [ObservableProperty] private ClearRecordMode _clearRecord = ClearRecordMode.Restarted;
+    [ObservableProperty] private int _halfRepeat = 1;
+    
     [ObservableProperty] private LotteryDrawType _drawType = LotteryDrawType.Pan;
     [ObservableProperty] private string _defaultPool = string.Empty;
     [ObservableProperty] private LotteryShowRandomMode _lotteryShowRandom = LotteryShowRandomMode.PrizeBreakGroupHyphenName;
